45f0a4c09aff97ae5dc3c56ca2ff68ccb60a8fc1
Karol Szpakowski <karol.szpakowski@gmail.com>
2022-03-06 17:36:21 +0100

    implement next dev tag

--- sort.sh
+++ sort.sh
@@ -14,30 +14,42 @@ function read_tags() {
 function next_dev_tag() {
     read_tags
     if [ -z $LAST_DEV_TAG ] ; then
+      echo "Last dev tag not found, returning default version"
       VERSION=$DEFAULT_TAG
     else
-      VERSION_ARRAY=(`echo $LAST_DEV_TAG | tr '.' ' '`)
-      # if number of elements is 3 this is old version pattern without patch - 0.1-dev.1
-      # if it's four then version is in new format with patch 0.1.0-dev.1
+      VERSION_BASE_AND_SUFFIX=(`echo $LAST_DEV_TAG | tr '-' ' '`)
+      VERSION_BASE="${VERSION_BASE_AND_SUFFIX[0]}"
+      SUFFIX=${VERSION_BASE_AND_SUFFIX[@]: -1:1}
+
+      echo "Base: $VERSION_BASE, Suffix: $SUFFIX"
+      
+      VERSION_ARRAY=(`echo $VERSION_BASE | tr '.' ' '`)
       MAJOR="${VERSION_ARRAY[0]}"
-      MINOR=$(echo ${VERSION_ARRAY[1]} | sed s/-dev//)
-      #TODO: detect RC for last dev and bump minor
-      # if echo $TAGS | grep -E "${VERSION_ARRAY[0]}\.${VERSION_ARRAY[1]}"..
-      #   NEXT_MINOR=$((${MINOR}+1))
-      #   VERSION=...
-      # ...
-      # TODO: else increment dev number
-      # else
-      #   VERSION_ARRAY=(`echo $LAST_DEV_TAG | tr '.' ' '`)
-      #   # if number of elements is 3 this is old version pattern without patch - 0.1-dev.1
-      #   # if it's four then version is in new format with patch 0.1.0-dev.1
-      #   echo "Number of elements in version array: ${#VERSION_ARRAY[@]}"
-      #   LAST_DEV_NUM=${VERSION_ARRAY[@]: -1:1}
-      #   echo "Last dev dev number: $LAST_DEV_NUM"
-      #   NEXT_DEV_NUM=$((${LAST_DEV_NUM}+1))
-      #   echo "Next dev number: ${NEXT_DEV_NUM}"
-      #   VERSION=$(echo $LAST_DEV_TAG | sed s/-dev.$LAST_DEV_NUM/-dev.$NEXT_DEV_NUM/)
-      # fi
+      MINOR="${VERSION_ARRAY[1]}"
+      PATCH="${VERSION_ARRAY[2]}"
+
+      if [ -z $PATCH ]; then
+        echo "Patch version part not found, defaulting to 0"
+        PATCH="0"
+      fi
+
+      echo "Major: $MAJOR, Minor: $MINOR, Patch: $PATCH"
+
+      MATCHING_RC=$(echo "$TAGS" | grep -E "$MAJOR\.$MINOR.?-RC\.")
+
+      if [ -z $MATCHING_RC ] ; then
+        echo "Matching RC version not found, incrementing dev version"
+        SUFFIX_ARRAY=(`echo $SUFFIX | tr '.' ' '`)
+        SUFFIX_VERSION=${SUFFIX_ARRAY[@]: -1:1}
+        NEXT_SUFFIX_VERSION=$((${SUFFIX_VERSION}+1))
+        echo "Suffix version: $NEXT_SUFFIX_VERSION"
+        VERSION="$MAJOR.$MINOR.$PATCH-dev.$NEXT_SUFFIX_VERSION"
+
+      else
+        echo "Found matching RC version: $MATCHING_RC, incrementing minor version"
+        NEXT_MINOR=$((${MINOR}+1))
+        VERSION="$MAJOR.$NEXT_MINOR.$PATCH-dev.1"
+      fi
     fi
 
     echo "Version: $VERSION"
@@ -79,7 +91,7 @@ read -r -d '' TAGS <<- EOM
     0.1-dev.9
 EOM
 next_dev_tag
-assertEq $VERSION "0.1-dev.12"
+assertEq $VERSION "0.1.0-dev.12"
 
 echo "----"
 echo "Should bump minor if RC tag is present"
@@ -98,4 +110,4 @@ read -r -d '' TAGS <<- EOM
     0.1-RC.1
 EOM
 next_dev_tag
-assertEq $VERSION "0.2-dev.1"
+assertEq $VERSION "0.2.0-dev.1"
